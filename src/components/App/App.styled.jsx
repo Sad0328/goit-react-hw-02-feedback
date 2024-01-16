db8147be4bf10f1aaf1cb60aeca6fae31f266d85
Sad0328 <tours@soledad.com.ua>
2024-01-16 19:35:00 +0200

    new2/2

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+`;
---
+++ src/components/FeedbackOpnions/FeedbackOptions.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  font-size: 20px;
+`;
